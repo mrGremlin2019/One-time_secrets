b7153dde9f05623b9accf7d71cba5c7c1d1f1fc1
mrGremlin2019 <vadim-ivanov01@mail.ru>
2025-04-09 19:51:37 +0300

    Удалены Docker-файлы из репозитория

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Ждем, пока БД и redis поднимутся
-echo "⌛ Ожидание PostgreSQL и Redis..."
-sleep 5
-
-# Стартуем приложение
-echo "🚀 Запуск uvicorn..."
-exec uvicorn main:app --host 0.0.0.0 --port 8000 --reload
